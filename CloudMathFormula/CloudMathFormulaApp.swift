0e963fb5ea5cca725445cb3713063c41cc2a31f0
Xun Ruan <xunruan@icloud.com>
2021-10-12 17:59:31 +0800

    Add Firebase to App

--- CloudMathFormula/CloudMathFormulaApp.swift
+++ CloudMathFormula/CloudMathFormulaApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
+
+// MARK: Configure the databse in the the didFinish event ( not in the init method in App class)
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
-struct CloudMathFormulaApp: App {
+struct CloudMathFormulaApp: App {   // Need a delegate to handle the launching app event
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
